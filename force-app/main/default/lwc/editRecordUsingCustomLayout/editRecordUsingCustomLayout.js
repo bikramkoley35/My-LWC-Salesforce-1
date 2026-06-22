2e59532f3dc002cbc3a0ce63d95d1fd4af55f37e
Bikram Koley <bikramkoley35@gmail.com>
2026-06-22 22:26:12 +0530

    Edit Record using Custom Layout

---
+++ force-app/main/default/lwc/editRecordUsingCustomLayout/editRecordUsingCustomLayout.js
@@ -0,0 +1,8 @@
+import { LightningElement, api } from 'lwc';
+
+export default class EditRecordUsingCustomLayout extends LightningElement {
+     @api recordId; // by only using this we can see whcih record id has which things only by opening it in salesforce after adding this from edit page
+        objectApiName = 'Case'; // If you by mistakenly put another object api name then it will automatically rectify you by telling which object holds that record Id....
+        //fields = ['CaseNumber', 'Priority', 'Status']; This is not needed for custom View record form
+        
+}
