c20326b1c39423cd5095d0e9698be09d8fa1b927
Bikram Koley <bikramkoley35@gmail.com>
2026-06-22 10:32:02 +0530

    loading custom record view form

---
+++ force-app/main/default/lwc/loadACustomViewRecord/loadACustomViewRecord.js
@@ -0,0 +1,7 @@
+import { LightningElement,api } from 'lwc';
+
+export default class LoadACustomViewRecord extends LightningElement {
+    @api recordId; // by only using this we can see whcih record id has which things only by opening it in salesforce after adding this from edit page
+    objectApiName = 'Case'; // If you by mistakenly put another object api name then it will automatically rectify you by telling which object holds that record Id....
+    //fields = ['CaseNumber', 'Priority', 'Status']; This is not needed for custom View record form
+}
