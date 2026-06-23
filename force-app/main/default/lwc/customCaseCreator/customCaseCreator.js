129ecbd098688d98090382cacb117326c858b0d1
Bikram Koley <bikramkoley35@gmail.com>
2026-06-23 20:08:25 +0530

    Using Wire creating record based on any custom record type

--- force-app/main/default/lwc/customCaseCreator/customCaseCreator.js
+++ force-app/main/default/lwc/customCaseCreator/customCaseCreator.js
@@ -1,18 +1,41 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 // for creating record i have to import createRecord function from uiRecord api module
 import { createRecord } from 'lightning/uiRecordApi';
+// To get object information directly we have to use getObjectInfo method
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
 // we have to import the object api name
 import CASE_OBJECT from '@salesforce/schema/Case';
 // For dynamically creation of fields
 import SUBJECT from '@salesforce/schema/Case.Subject';
 import PRIORITY from '@salesforce/schema/Case.Priority';
 import DESC from '@salesforce/schema/Case.Description';
+import RECID from '@salesforce/schema/Case.RecordTypeId';
 
 export default class CustomCaseCreator extends LightningElement {
     // at first variable creation needed
     subject ='';
     priority='';
     description='';
+    recordTypeId='';
+
+    // To fetch the data from object api info we have to use wire method
+    @wire(getObjectInfo,{
+        objectApiName : CASE_OBJECT
+    })caseRecord({data,error}){ // as we know this variable consist data and error we are doing work with data now..
+        if(data){
+            console.log('Data recieved: '+JSON.stringify(data)); // at first we do a console log from there we understood that there is two type of recordInfos
+            let recordTypeDetails = data.recordTypeInfos; // we stored those here in a object named recordTypeDetails
+            Object.keys(recordTypeDetails).forEach((key) => { // object.keys enables to look upon the keys of any object and forEach is the loop and key is the variable we created we could have take any other name also
+                const recordTypeInfo = recordTypeDetails[key]; // now for every iteration it stores the record based on the key value untill we are not finding that one
+                if(recordTypeInfo.name == 'SMS_Community_Cases'){ // whose name matches with this record type info name
+                    this.recordTypeId = recordTypeInfo.recordTypeId; // from here we fetched the recordtype id of that info
+                }
+            })
+        }
+    }; 
+    
+    // we are fetching info from Case object and storing it in caseRecord Variable
+    // Now from Case record I want to get only recordTypeName
     // Getter for Options For Priority field
     get options() {
         return [
@@ -41,6 +64,7 @@ export default class CustomCaseCreator extends LightningElement {
         fields[SUBJECT.fieldApiName]=this.subject;
         fields[PRIORITY.fieldApiName]=this.priority;
         fields[DESC.fieldApiName]=this.description;
+        fields[RECID.fieldApiName]=this.recordTypeId;
         // I have to pass here object api name and the list of fields...
         let recordInput={apiName : CASE_OBJECT.objectApiName, fields};
         await createRecord(recordInput)  // Await basically tells that system have to wait untill this method runs...
@@ -53,3 +77,6 @@ export default class CustomCaseCreator extends LightningElement {
         // handling error here.. 
     }
 }
+
+// we can see the record type in Developer Console by using this query
+// select id, subject, priority, recordType.Name from case where Createddate = today
